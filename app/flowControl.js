348116e811010915b9d546e878d91a9454a0f726
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2019-05-26 13:35:58 -0500

    add flow control and functions answers

--- app/flowControl.js
+++ app/flowControl.js
@@ -2,6 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.flowControlAnswers = {
   fizzBuzz: function(num) {
+    by3 = num % 3 === 0;
+    by5 = num % 5 === 0;
+    by3And5 = by3 && by5;
+    if (by3And5) {
+    	return 'fizzbuzz';
+    } 
+  else if (by3) {
+   	return 'fizz';
+   } 
+  else if (by5) {
+  	return 'buzz';
+   } else if (isNaN(num)) {
+  	return false;
+  } else {
+  	return num;
+  } 
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
--- app/functions.js
+++ app/functions.js
@@ -2,38 +2,75 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+	  return fn.apply(null, arr);
   },
 
   speak: function(fn, obj) {
-
+          return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+	  return function(arg){
+	  	return str + ", " + arg;
+	  }
   },
 
   makeClosures: function(arr, fn) {
+  	var functionArray = [];
+	var createFunction = function(value){
+		return function(){
+			return fn(value);
+		} 
+	} 
+	for(var i = 0; i<arr.length; i++){
+		functionArray.push(createFunction(arr[i]));
 
+	}
+  	return functionArray;
   },
 
   partial: function(fn, str1, str2) {
-
+  	
+  	return function(str3){
+		return fn(str1, str2, str3);
+	}
   },
 
   useArguments: function() {
-
+	var total = 0;
+        for(var i = 0; i<arguments.length; i++){
+		total+= arguments[i];
+	}
+	return total;
   },
 
   callIt: function(fn) {
+	  var argumentsArray = [];
+	  //The first argument is the function to be passed
+	  for(var i = 1; i<arguments.length; i++){
+  	  	argumentsArray.push(arguments[i]);	
+	  };
+	  return fn.apply(null, argumentsArray);
 
   },
-
   partialUsingArguments: function(fn) {
-
+    var slice = Array.prototype.slice;
+    var args = slice.call(arguments, 1);
+    return function() {
+    return fn.apply(this, args.concat(slice.call(arguments, 0)));}
   },
 
   curryIt: function(fn) {
-
+    var slice = Array.prototype.slice;
+    var args = slice.call(arguments, 1);
+    function resolver(num){
+        args.push(num);
+        if(args.length>=fn.length){
+            return fn.apply(null, args);        
+        }
+        return resolver;
+    }
+    return resolver;
+    
   }
 };
