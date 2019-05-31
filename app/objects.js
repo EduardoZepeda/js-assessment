d26533d8624e88bf14049d4603151a62e05fbb9a
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2019-05-31 10:52:44 -0500

    add objects and recursion answers

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
+    return 0;
   },
 
   iterate: function(obj) {
+    var listOfProperties = [];
 
+    for (var property in obj) {
+      if (obj.hasOwnProperty(property)) {
+        listOfProperties.push(property + ': ' + obj[property]);
+      }
+    }
+
+    return listOfProperties;
   }
 };
--- app/recursion.js
+++ app/recursion.js
@@ -1,7 +1,22 @@
 exports = typeof window === 'undefined' ? global : window;
-
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var addFiles = true;
+    if(dirName != undefined && dirName != data.dir){
+        addFiles = false;
+    }
+    var FilesInData = [];
+    for(let f of data.files){
+      if(f.hasOwnProperty('files')){
+           if(addFiles){dirName = f.dir}
+           FilesInData = FilesInData.concat(this.listFiles(f, dirName));
+      }else{
+       if(addFiles){
+	     FilesInData = FilesInData.concat(f);
+       }
+      }
+    } 
+    return FilesInData
 
   },
 
