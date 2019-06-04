2dc2ec9d9fe7144d9281b110cb4b76f52bfcf062
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2019-06-04 11:33:48 -0500

    add regex and string answers

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,37 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    var regNum = /\d+/;
+    return regNum.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    var repeatLetter = /([a-zA-Z])\1{1,}/g; 
+    return repeatLetter.test(str);
   },
 
   endsWithVowel: function(str) {
-
+    var endsVowel = /[aeiouAEIOU]$/
+    return endsVowel.test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    var ThreeNumbers = /\d{3}/
+    var result = ThreeNumbers.exec(str);
+    if(result){
+       return result[0];}
+    else {
+       return false;
+    }
   },
 
   matchesPattern: function(str) {
-
+     var pattern = /^\d{3}-\d{3}-\d{4}$/
+     return pattern.test(str);
   },
 
   isUSD: function(str) {
-
+      var usdPattern = /^\$\d{1,3},?(\d{3},?)*(\.\d{2})?$/ 
+      return usdPattern.test(str);
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,32 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    var lettersArray = str.match(/([a-zA-Z])\1{0,}/g);
+    var newText = "";
+    for(let element of lettersArray){
+        newText += element.slice(0, amount);
+        }
+    return newText;
   },
 
   wordWrap: function(str, cols) {
-
+    var stringArrays = str.split(" ");
+    var formattedString = stringArrays.reduce(function(prev, post){
+	if(prev.length + post.length>cols){
+		prev+="\n";
+    }else{
+        prev+=" ";
+    }
+    return prev + post;});
+    return formattedString;
   },
 
   reverseString: function(str) {
+    var reversedString = "";
+    for(let i=str.length-1; i>=0; i--){
+        reversedString+= str[i];
 
+        }
+    return reversedString;
   }
 };
